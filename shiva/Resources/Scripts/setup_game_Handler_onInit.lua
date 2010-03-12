a56376161f7c2a2db36f6504551999f1cb50c489
Taro Omiya <japtar10101@gmail.com>
2010-03-12 11:46:28 -0500

    Attempted to add a sword in shiva game.

--- shiva/Resources/Scripts/setup_game_Handler_onInit.lua
+++ shiva/Resources/Scripts/setup_game_Handler_onInit.lua
@@ -9,6 +9,11 @@ function setup_game.onInit (  )
 --------------------------------------------------------------------------------
 	-- Get to the first default scene
     application.setCurrentUserScene( "city_scape" )
+    
+    -- Set the camera's position
+    local mainCamera = application.getCurrentUserActiveCamera()
+    object.setTranslation ( mainCamera, 0, 0, 0, object.kGlobalSpace )
+    object.lookAt ( mainCamera, 0, 0, -10, object.kGlobalSpace, 1 )
 --------------------------------------------------------------------------------
 end
 --------------------------------------------------------------------------------
