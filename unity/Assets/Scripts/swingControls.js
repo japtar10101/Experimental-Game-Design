ef07bab1ac7d9b0fd5ae9b83c0114a5f2cf94feb
Taro Omiya <japtar10101@gmail.com>
2010-03-12 22:03:39 -0500

    Added invisible walls.  Prevents player from exceeding the screen limits (and the cityscape, when we add it in later)

--- unity/Assets/Scripts/swingControls.js
+++ unity/Assets/Scripts/swingControls.js
@@ -5,25 +5,36 @@ var toRotate : Transform;
 private var isAnimated = false;
 private var rotateAxis = Vector3.zero;
 private var angle = 0;
+private var totalAngle = 0;
 
 function Update () {
 	if( !rotateAround || !toRotate ) {
 		return;
 	}
-	// Figure out the direction to rotate in
+	//TODO: deal with animation
+	/*
+	else if( !isAnimated ) {
+		angle = 0;
+	}
+	*/
 	angle = 0;
+	// Figure out the direction to rotate in
 	if( Input.GetKey( KeyCode.UpArrow ) ) {
 		rotateAxis = Vector3.right;
 		angle -= 1;
+		isAnimated = true;
 	} else if( Input.GetKey( KeyCode.DownArrow ) ) {
 		rotateAxis = Vector3.right;
 		angle += 1;
+		isAnimated = true;
 	} else if( Input.GetKey( KeyCode.RightArrow ) ) {
 		rotateAxis = Vector3.up;
 		angle += 1;
+		isAnimated = true;
 	} else if( Input.GetKey( KeyCode.LeftArrow ) ) {
 		rotateAxis = Vector3.up;
 		angle -= 1;
+		isAnimated = true;
 	}
 	
 	// Move the controller
