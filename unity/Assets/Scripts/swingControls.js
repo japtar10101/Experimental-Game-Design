c79fe7e4acad7b6658aac1a478e3b88bebd9270e
Taro Omiya <japtar10101@gmail.com>
2010-03-12 23:25:04 -0500

    Finally implemented sword-swinging.  Controls are WASD to move the box, and arrow keys to swing the sword.

--- unity/Assets/Scripts/swingControls.js
+++ unity/Assets/Scripts/swingControls.js
@@ -1,6 +1,8 @@
-var speed = 250.0;
+var speed = 900.0;
 var rotateAround : Transform;
 var toRotate : Transform;
+var defaultPosition : Vector3 = new Vector3(0,0,-10);
+var defaultAngle : Vector3 = new Vector3(90,0,0);
 
 private var isAnimated = false;
 private var rotateAxis = Vector3.zero;
@@ -9,23 +11,23 @@ private var totalAngle = 0;
 
 function Update () {
 	if( !rotateAround || !toRotate ) {
+		// If Transforms aren't defined, halt
+		return;
+	} else if( isAnimated ) {
+		//If we're animated, continue animating
+		animateSwing();
 		return;
 	}
-	//TODO: deal with animation
-	/*
-	else if( !isAnimated ) {
-		angle = 0;
-	}
-	*/
-	angle = 0;
+		
 	// Figure out the direction to rotate in
+	setupAnimation();
 	if( Input.GetKey( KeyCode.UpArrow ) ) {
 		rotateAxis = Vector3.right;
-		angle -= 1;
+		angle += 1;
 		isAnimated = true;
 	} else if( Input.GetKey( KeyCode.DownArrow ) ) {
 		rotateAxis = Vector3.right;
-		angle += 1;
+		angle -= 1;
 		isAnimated = true;
 	} else if( Input.GetKey( KeyCode.RightArrow ) ) {
 		rotateAxis = Vector3.up;
@@ -36,10 +38,22 @@ function Update () {
 		angle -= 1;
 		isAnimated = true;
 	}
-	
-	// Move the controller
 	angle *= speed;
-	toRotate.RotateAround( rotateAround.position, rotateAxis, angle * Time.deltaTime);
 }
 
-@script RequireComponent(Transform)
+function setupAnimation() {
+	angle = 0;
+	totalAngle = 0;
+	toRotate.position = rotateAround.TransformPoint( defaultPosition );
+	toRotate.localEulerAngles = defaultAngle;
+}
+
+function animateSwing() {
+	var incAngle = angle * Time.deltaTime;
+	totalAngle += incAngle;
+	if( totalAngle > 360 || totalAngle < -360 ) {
+		isAnimated = false;
+	} else {
+		toRotate.RotateAround( rotateAround.position, rotateAxis, incAngle);
+	}
+}
