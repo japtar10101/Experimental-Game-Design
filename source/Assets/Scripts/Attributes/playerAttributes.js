ca5eba9ffd5c44c4ccae0d06b3c06077e65481d1
unknown <oparow@.(none)>
2010-03-16 22:13:08 -0400

    Adding player updating script.

--- source/Assets/Scripts/Attributes/playerAttributes.js
+++ source/Assets/Scripts/Attributes/playerAttributes.js
@@ -12,8 +12,12 @@ var maxShield = 100;
 private var shieldDuration = 100;
 private var shieldOn = false;
 
+//Health stuff
+//private var healthBar = GUI.Label(Rect(10,10,100,20),"Please work!");
+
 function updateHealth( changeHealth ) {
 	health += changeHealth;
+	//GUI.Label(Rect(10,10,100,20), "Hit!");
 	if( health > maxShield ) {
 		health = maxShield;
 	}
@@ -34,4 +38,23 @@ function isShieldOn() {
 function Start() {
 	health = maxHealth;
 	shieldDuration = maxShield;
+	//GUI.Label(Rect(10, 10, 100, 20), "Not Hit");
+	//OnGui();
+}
+
+function OnGui() {
+	GUI.Label(Rect(0,0,100,20), "health");
+}
+
+function OnTriggerEnter (other : Collider) {
+	//check to make sure an enemy is colliding
+	var collided = other.gameObject;
+	if( collided.CompareTag("Destructable") && 
+collided.renderer.enabled) {
+		if(updateHealth(-40)) {
+			Destroy(gameObject);
+			Application.Quit();
+		}
+		Destroy(collided);
+	}
 }
